f22fa3e4aec898d17fbbb7c50c8cfad7abf437ec
4pablo124 <pablo.delosriosges@gmail.com>
2021-08-07 20:49:28 +0200

    Residseñado creación y unirse a sala

--- Assets/ARCall/Scripts/RoomSelection/RoomLoader.cs
+++ Assets/ARCall/Scripts/RoomSelection/RoomLoader.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 public class RoomLoader : MonoBehaviour
 {   
     public PeerType peerType = PeerType.Host;
-    public TextMeshProUGUI roomIDText;
-    public TMP_InputField roomIDInput;
-    public TextMeshProUGUI errorText;
-    public Button joinBtn;
+    private TextMeshProUGUI roomIDText;
+    private TMP_InputField roomIDInput;
+    private Button joinBtn;
+    private Button shareBtn;
 
 
     private void Awake() {
         roomIDText = GameObject.Find("RoomIDText")?.GetComponent<TextMeshProUGUI>();
         roomIDInput = GameObject.Find("RoomIDInput")?.GetComponent<TMP_InputField>();
-        errorText = GameObject.Find("ErrorText")?.GetComponent<TextMeshProUGUI>();
         joinBtn = GameObject.Find("JoinBtn").GetComponent<Button>();
+        shareBtn = GameObject.Find("ShareBtn")?.GetComponent<Button>();
     }
 
     // Start is called before the first frame update
@@ -24,22 +24,36 @@ public class RoomLoader : MonoBehaviour
         if(peerType == PeerType.Host){
             RoomManager.GenerateRoomID().ContinueWithOnMainThread( roomIDTask => {
                 roomIDText.text = roomIDTask.Result;
-                joinBtn.interactable = true;
             });
+            shareBtn.onClick.AddListener(() => Sharing.ShareRoom());
+            joinBtn.onClick.AddListener(() => JoinRoom(roomIDText.text));
+        }else{
+            joinBtn.onClick.AddListener(() => JoinRoom(roomIDInput.text));
+            roomIDInput.onSubmit.AddListener((roomID) => JoinRoom(roomID));
         }
 
-        joinBtn.onClick.AddListener(() => {
-            //TODO: incluir en JoinRoom el id 
-            if(peerType == PeerType.Client) RoomManager.RoomID = roomIDInput.text;
-            RoomManager.JoinRoom(peerType).ContinueWithOnMainThread(success =>{
-                errorText.enabled = !success.Result;
-            });
-        });
+
     }
 
     private void Update() {
+        if(peerType == PeerType.Host) shareBtn.interactable = IsValidRoomCode();
+        joinBtn.interactable = IsValidRoomCode();
+    }
+
+    void JoinRoom(string roomID){
+        RoomManager.RoomID = roomID;
+        RoomManager.JoinRoom(peerType).ContinueWithOnMainThread(success =>{
+            if(!success.Result){
+                AndroidUtils.ShowToast("¡Código de sala incorrecto!");
+            }
+        });
+    }
+
+    bool IsValidRoomCode(){
         if(peerType == PeerType.Client){
-            joinBtn.interactable = roomIDInput.text.Length == 4;
+            return roomIDInput.text.Length == 4;
+        }else{
+            return roomIDText.text != "----";
         }
     }
 }
