0aac7cce386bb23a153e2f33b82c213107774f4c
4pablo124 <pablo.delosriosges@gmail.com>
2021-08-07 20:48:38 +0200

    Rediseñado Registro

--- Assets/ARCall/Scripts/SignIn/NameSetUp.cs
+++ Assets/ARCall/Scripts/SignIn/NameSetUp.cs
@@ -6,24 +6,39 @@ using Firebase.Extensions;
 public class NameSetUp : MonoBehaviour
 {
 
-    public TMP_InputField nameInput;
-    public Button registerButton;
+    private TMP_InputField nameInput;
+    private Button registerBtn;
 
+
+    private void Awake() {
+        nameInput = GameObject.Find("NameInput").GetComponent<TMP_InputField>();
+        registerBtn = GameObject.Find("RegisterBtn").GetComponent<Button>();
+    }
+
+    private void Start() {
+        registerBtn.onClick.AddListener(() => RegisterName(nameInput.text));
+        nameInput.onSubmit.AddListener((name) => {if(IsValidNameInput()) RegisterName(name);});
+    }
 
     // Update is called once per frame
     void Update()
     {   
-        registerButton.interactable = nameInput.text == "" ? false : true;
+        registerBtn.interactable = IsValidNameInput();
     }
 
-    public void register(){
+
+    bool IsValidNameInput(){
+        return nameInput.text != "";
+    }
+
+    public void RegisterName(string name){
         if(AuthManager.IsUserRegistered()){
-            AuthManager.ChangeUsername(nameInput.text).ContinueWithOnMainThread(task => {
+            AuthManager.ChangeUsername(name).ContinueWithOnMainThread(task => {
                 Debug.Log(AuthManager.Auth.CurrentUser.DisplayName);
                 UISceneNav.LoadScene("Main");
             });
         }else{
-            AuthManager.SignUp(nameInput.text).ContinueWithOnMainThread(task => {
+            AuthManager.SignUp(name).ContinueWithOnMainThread(task => {
                 Debug.Log(AuthManager.Auth.CurrentUser.DisplayName);
                 UISceneNav.LoadScene("Main");
             });
